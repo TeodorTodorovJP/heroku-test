f21e2a85197af4c40dd53e6bb4b8a3169bd6d764
Teodor Todorov <ttodorov.jp@gmail.com>
2022-04-21 12:14:49 +0300

    Small changes

--- heroku-test-app/src/App.js
+++ heroku-test-app/src/App.js
@@ -1,13 +1,40 @@
+import React, { useCallback, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
+  const [url, setUrl] = useState('/api');
+
+  const fetchData = useCallback(() => {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(json => {
+        setMessage(json.message);
+        setIsFetching(false);
+      }).catch(e => {
+        setMessage(`API call failed: ${e}`);
+        setIsFetching(false);
+      })
+  }, [url]);
+
+  useEffect(() => {
+    setIsFetching(true);
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {message}
         </p>
         <a
           className="App-link"
